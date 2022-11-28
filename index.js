61f4081d4396c0063633155a4c048f566ba7da47
Rigoberto Alcantar Aguilar <a20150338@utem.edu.mx>
2022-11-28 17:20:58 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const http = require("http")
+const cors = require('cors')
+const { Server } = require("socket.io")
+
+const PORT = 3001
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`)
+
+    socket.on("send_remove_ticket", (data) =>{
+        console.log('send_remove_ticket',data)
+        socket.to('SNHD').emit("recive_remove_ticket", data)
+    })
+
+
+
+
+
+    socket.on("join_room", (data) => {
+        socket.join(data)
+        console.log(`User ID: ${socket.id} joined to: ${data}`)
+    })
+
+    socket.on("send_message", (data) =>{
+        console.log('send_message',data)
+        socket.to(data.room).emit("receive_message", data)
+    })
+
+    socket.on("disconnect", () => {
+        console.log('User disconnected', socket.id)
+    })
+})
+
+server.listen(PORT, () => {
+    console.log('SERVER RUNNING IN PORT: ', PORT)
+})
